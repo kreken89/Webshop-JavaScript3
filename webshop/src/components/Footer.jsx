888f213f4e42c5aaa5283b42550f87d8c9b65c5e
Gustav Clavin <gustav.clavin99@gmail.com>
2023-05-05 16:05:11 +0200

    Footer done?

--- webshop/src/components/Footer.jsx
+++ webshop/src/components/Footer.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 
+import { FaFacebook, FaLinkedinIn } from 'react-icons/fa';
+import { BsTwitter, BsPinterest } from 'react-icons/bs'
+
 const Footer = () => {
   return (
-    <div class="footer">
-      <div class="footer-top">
-        <div class="footer-general footer-content-container">
-          <h3>LOGO</h3>
+    <div className="footer">
+      <div className="footer-top">
+        <div className="footer-general footer-content-container">
+          <img src="../../public/logo.png" alt="Our logo"></img>
           <h4>869, Miyan road, Y4 456 Mona Town.</h4>
           <h4>Office No 3456.</h4>
           <h4>info@example.com</h4>
           <h4>+234-58286909</h4>
         </div>
-        <div class="footer-links footer-content-container">
+        <div className="footer-links footer-content-container">
           <h3>Links.</h3>
           <a href="">LED light source</a>
           <a href="">Pendant Lamp</a>
@@ -19,7 +22,7 @@ const Footer = () => {
           <a href="">LED Pendant Lamp</a>
           <a href="">Table Lamp</a>
         </div>
-        <div class="footer-about footer-content-container">
+        <div className="footer-about footer-content-container">
         <h3>About Us.</h3>
           <a href="">About Us</a>
           <a href="">Blog</a>
@@ -27,18 +30,18 @@ const Footer = () => {
           <a href="">Sign in</a>
           <a href="">My account</a>
         </div>
-        <div class="footer-contact footer-content-container">
-        <h3>Contact Us.</h3>
+        <div className="footer-contact footer-content-container">
+          <h3>Contact Us.</h3>
           <p>We are availble eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est</p>
-          <div>
-            <i>Facebook</i>
-            <i>Twitter</i>
-            <i>Linked In</i>
-            <i>Pinterest</i>
+          <div className="social-icons">
+            <span><FaFacebook /></span>
+            <span><BsTwitter /></span>
+            <span><FaLinkedinIn /></span>
+            <span><BsPinterest /></span>
           </div>
         </div>
       </div>
-      <div class="footer-bottom">©2020 CopyRight Example. All rights reserved.</div>
+      <div className="footer-bottom">©2020 CopyRight Example. All rights reserved.</div>
       <div></div>
     </div>
   )
