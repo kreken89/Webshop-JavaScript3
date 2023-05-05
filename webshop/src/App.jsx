f274dd6e88ab06a9e0ad06736d848c4bd48ff7de
Gustav Clavin <gustav.clavin99@gmail.com>
2023-05-05 13:43:13 +0200

    added sass

--- webshop/src/App.jsx
+++ webshop/src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-<<<<<<< HEAD
 import Footer from './components/Footer'
-=======
 import './App.scss'
 
->>>>>>> 100dd74a316ddeb3dc3b42a410931ca57be4a679
 
 const App = () => {
   
