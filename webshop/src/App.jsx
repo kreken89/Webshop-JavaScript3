466e96bb1bd195c2ac32b8017b5fc0716bceebee
DVNO <dvno@live.se>
2023-05-05 16:28:29 +0200

    small changes

--- webshop/src/App.jsx
+++ webshop/src/App.jsx
@@ -1,31 +1,24 @@
 import React from 'react'
 import Header from './components/Header'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import Home from './pages/home/Home'
 import Contact from './pages/contact/Contact'
 import './App.scss'
 
-
 const App = () => {
-  
   return (
-
     <>
- <Router>
-    <Header/>
-    <Routes>
-    
-    <Route path='/' element={<Home/>}/>
-    <Route path='/' element={<Contact/>}/>
-    
-
-    </Routes>
-    <Footer/>
-  </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   )
-  
 }
 
 export default App
