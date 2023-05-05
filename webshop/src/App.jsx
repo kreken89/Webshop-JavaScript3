008275d69d62894de53828e6ab972695c1d0b2ec
Gustav Clavin <gustav.clavin99@gmail.com>
2023-05-05 16:07:37 +0200

    Nasjs

--- webshop/src/App.jsx
+++ webshop/src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+
+import Footer from './components/Footer'
+
 import './App.scss'
 
 
 const App = () => {
   
   return (
-    <div>App aaaaaa</div>
+
+    <>
+ <Router>
+    <Header/>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/' element={<Contact/>}/>
+
+
+    </Routes>
+  </Router>
+    </>
   )
+  
 }
 
 export default App
