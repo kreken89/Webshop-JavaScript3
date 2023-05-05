341696d7c9843fc7fb43335e2d64136631f75fb1
DVNO <dvno@live.se>
2023-05-05 13:43:40 +0200

    bugg fix

--- webshop/src/App.jsx
+++ webshop/src/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
-<<<<<<< HEAD
-import Footer from './components/Footer'
-=======
 import './App.scss'
 
->>>>>>> 100dd74a316ddeb3dc3b42a410931ca57be4a679
 
 const App = () => {
   
