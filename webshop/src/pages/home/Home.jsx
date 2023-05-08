0d359f1cee9b839e2292cbe10707e08e700c609f
Mikael Krefors <krefors@hotmail.com>
2023-05-08 14:06:57 +0200

    Minor changes on master

--- webshop/src/pages/home/Home.jsx
+++ webshop/src/pages/home/Home.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
 import Hero from '../../components/hero/Hero'
 import BestCollection from '../../components/bestCollection/BestCollection'
-<<<<<<< HEAD
-// import BestCollectionCard from '../../components/bestCollection/BestCollectionCard'
-=======
 import Campaign from '../../components/campaign/Campaign'
 import TopSellingProducts from '../../components/topSellingProducts/TopSellingProducts'
 
->>>>>>> 00f61cfbbd5daf16938ef725427222b7c5358203
 
 const Home = () => {
   return (
     <div className="home_container">
       <Hero />
-      <div className="best_colection">
+      <div className="best_collection">
       <BestCollection />
       </div>
       <Campaign />
