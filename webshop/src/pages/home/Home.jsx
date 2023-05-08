00f61cfbbd5daf16938ef725427222b7c5358203
Mikael Krefors <krefors@hotmail.com>
2023-05-08 13:59:28 +0200

    Added campaigns and top products components

--- webshop/src/pages/home/Home.jsx
+++ webshop/src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Hero from '../../components/hero/Hero'
 import BestCollection from '../../components/bestCollection/BestCollection'
+import Campaign from '../../components/campaign/Campaign'
+import TopSellingProducts from '../../components/topSellingProducts/TopSellingProducts'
+
 
 const Home = () => {
   return (
     <div className="home_container">
       <Hero />
       <BestCollection />
+      <Campaign />
+      <TopSellingProducts />
     </div>
   )
 }
