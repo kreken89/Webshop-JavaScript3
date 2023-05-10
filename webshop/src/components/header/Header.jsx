d52178f69644f2c8a6f038ac4ace46e6608e1908
DVNO <dvno@live.se>
2023-05-10 15:40:18 +0200

    navbar almost done

--- webshop/src/components/header/Header.jsx
+++ webshop/src/components/header/Header.jsx
@@ -1,56 +1,49 @@
 import React from 'react'
 import styles from './Header.module.scss'
-import logo from "../../assets/placeholders/Logo.svg"
-import {Link, NavLink} from 'react-router-dom'
-import {FaShoppingCart} from "react-icons/fa"
+import logo from '../../assets/placeholders/Logo.svg'
+import { Link, NavLink } from 'react-router-dom'
+import { FaShoppingCart } from 'react-icons/fa'
 
-const Header = ({user}) => {
+const Header = ({ user }) => {
   return (
     <>
-    <header>
-      <div className={styles.header}>
-    <img src={logo} alt="" />
-       <nav >
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-          <li>
-            <Link to="/products">Products</Link>
-          </li>
-        </ul>
-       
-        <div className={styles["header-right"]}>
-          <span className={styles.links}>
-          {user ? (
-          <>
-          <NavLink to="/orders">My orders</NavLink>
-          <NavLink to="/">Logout</NavLink>
-          </>
-          ) : (
-            <>
-            
-            <Link to="/login">Login</Link>
-            <Link to="/register"> Register</Link>
-            </>
-
-        )}
-      
-
-            
-          </span>
-          <span className={styles.cart}>
-          <Link to="/cart"><FaShoppingCart/></Link>
-          </span>
-         
-        </div>
-       </nav>
-      </div>
-    </header>
-
+      <header>
+        <nav className="nav_container">
+          <div className="logo_div">
+            <img src={logo} alt="" />
+          </div>
+          <div className="nav_links_div">
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/products">Products</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+              <li>
+                <NavLink to="/register">Register</NavLink>
+              </li>
+              <li>
+                <NavLink to="/">Logout</NavLink>
+              </li>
+              <li>
+                <NavLink to="/orders">My orders</NavLink>
+              </li>
+            </ul>
+            <span className={styles.cart}>
+              <Link to="/cart">
+                <FaShoppingCart />
+              </Link>
+            </span>
+          </div>
+        </nav>
+      </header>
     </>
   )
 }
