c9ed4bdfdb522e0c1ba7f2f106010a6e606db955
DVNO <dvno@live.se>
2023-05-10 17:56:00 +0200

    Start whit register and login page

--- webshop/src/App.jsx
+++ webshop/src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/header/Header'
-import Products from './pages/products/Products'
-import Footer from './components/footer/Footer'
 import './App.scss'
-import Subscription from './components/subscribe/Subscription'
-import ProductDetails from './pages/productDetails/ProductDetails'
-
 
 // Pages
 import Home from './pages/home/Home'
+import Products from './pages/products/Products'
 import Contact from './pages/contact/Contact'
+import ProductDetails from './pages/productDetails/ProductDetails'
+import LoginSignIn from './pages/loginSignIn/LoginSignIn'
+
+// Components
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Subscription from './components/subscribe/Subscription'
 
 
 
@@ -23,6 +25,7 @@ const App = () => {
           <Route path="/contact" element={<Contact />} />
           <Route path="/productDetails" element={<ProductDetails />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/login" element={<LoginSignIn />} />
         </Routes>
         <Subscription />
         <Footer />
---
+++ webshop/src/pages/loginSignIn/LoginSignIn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginSignIn = () => {
+  return (
+    <div>LoginSignIn</div>
+  )
+}
+
+export default LoginSignIn
