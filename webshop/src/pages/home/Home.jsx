11eeae283ef28987f701bbeb94401f08ffa0712b
DVNO <dvno@live.se>
2023-05-10 09:41:31 +0200

    xo

--- webshop/src/pages/home/Home.jsx
+++ webshop/src/pages/home/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Hero from '../../components/hero/Hero'
 import BestCollection from '../../components/bestCollection/BestCollection'
 import Campaign from '../../components/campaign/Campaign'
-// import TopSellingProducts from '../../components/topSellingProducts/TopSellingProducts'
 import Carousel from '../../components/slider/Carousel'
 
 const Home = () => {
