ef0a9b628e3cef3f766f6a33fb8f9895271496d2
Elin <es.fredriksson@gmail.com>
2023-05-10 13:20:54 +0200

    Contact

--- webshop/src/pages/contact/Contact.jsx
+++ webshop/src/pages/contact/Contact.jsx
@@ -1,48 +1,50 @@
 import React from 'react'
-import Header from '../../components/header/Header'
+
+
+
 
 const Contact = () => {
   return (
     <>
     <section class="contact-wrap">
-      <h3>Contact us!</h3>
-  <form action="" class="contact-form">
-    <div class="">
-      <div class="">
+  <form action="#" class="contact-form">
+      <h3 className='title'>Contact us!</h3>
+    <div class="user-details">
+      <div class="input-box">
         <label for="fullName">Your Name <span className='required'>*</span></label>
-        <input type="text" id="fullName" class="form-control"/>
+        <input type="text" id="fullName" class="form-control" required/>
       </div>
-    </div>
-    <div class="">
-      <div class="">
+      <div class="input-box">
         <label for="email">Your Email <span className='required'>*</span></label>
-        <input type="email" id="email" class="form-control"/>
+        <input type="email" id="email" class="form-control" required/>
       </div>
-    </div>
-    <div class="">
-      <div class="">
-        <label for="phoneNumber">Phone Number<span className='required'>*</span></label>
-        <input type="number" id="phoneNumber" class="form-control"/>
+      <div class="input-box">
+        <label for="phoneNumber">Phone Number <span className='required'>*</span></label>
+        <input type="number" id="phoneNumber" class="form-control" required/>
       </div>
-    </div>
-    <div class="">
-      <div class="">
+      <div class="input-box">
         <label for="company">Company <span className='optinal'>(optinal)</span></label>
         <input type="text" id="company" class="form-control"/>
       </div>
-    </div>
-    <div class="">
-      <div class="">
+      <div class="input-box">
         <label for="message">Write something <span className='required'>*</span></label>
-        <textarea rows="3" type="text" id='message' class="form-control"></textarea>
+        <textarea rows="6" type="text" id='message' class="form-control text-area" required></textarea>
       </div>
     </div>
+    <div className="terms">
+      <input type="checkbox" />
+      <label htmlFor=""> I accept to save my personal details</label>
+      
+    </div>
     
-      <button class="submit-btn">Send</button>
+    
+    
+    
+      <button class="submit-btn">Submit</button>
     
   </form>
-</section>
 
+</section>
      
     </>
   )
