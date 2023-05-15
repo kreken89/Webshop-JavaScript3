dd4e8c1952ad5eb7bbf845e78489bebd082ca228
DVNO <dvno@live.se>
2023-05-15 14:06:09 +0200

    xo

--- webshop/src/components/registerForm/RegisterForm.jsx
+++ webshop/src/components/registerForm/RegisterForm.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import { collection, addDoc } from 'firebase/firestore'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { auth } from '../../firebase/utils'
+import { auth, db } from '../../firebase/utils'
 import { useState } from 'react'
 
 const RegisterForm = () => {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [address, setAddress] = useState('')
+  const [city, setCity] = useState('')
   const [postalCode, setPostalCode] = useState('')
   const [phoneNumber, setPhoneNumber] = useState('')
   const [email, setEmail] = useState('')
@@ -21,25 +22,37 @@ const RegisterForm = () => {
       return
     }
 
-    try{
-      const { user } = await createUserWithEmailAndPassword(auth, email, password)
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+    
+      console.log(user)
 
       const userData = {
         firstName,
         lastName,
+        email,
+        city,
         address,
         postalCode,
         phoneNumber,
       }
 
+      // Save userData to Firestore
+      const docRef = await addDoc(collection(db, 'users'), userData)
+      console.log('User document written with ID: ', docRef.id)
+
       // Save userData to Firebase database or Firestore
-      console.log(userData);
+      console.log(userData)
 
       // Optionally, you can redirect the user to another page
       // after successful registration
       // history.push('/dashboard');
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   }
   return (
@@ -55,13 +68,107 @@ const RegisterForm = () => {
           value={firstName}
           onChange={(e) => setFirstName(e.target.value)}
         />
+
+        <label htmlFor="lastName">
+          Enter Your Last Name <span className="required">*</span>
+        </label>
+        <input
+          type="text"
+          id="lastName"
+          className="form-control"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+
+        <label htmlFor="address">
+          Enter Your Address <span className="required">*</span>
+        </label>
+        <input
+          type="text"
+          id="address"
+          className="form-control"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+
+        <label htmlFor="city">
+          Enter Your City <span className="required">*</span>
+        </label>
+        <input
+          type="text"
+          id="city"
+          className="form-control"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+
+        <label htmlFor="postal_code">
+          Postal Code <span className="required">*</span>
+        </label>
+        <input
+          type="text"
+          id="postal_code"
+          className="form-control"
+          value={postalCode}
+          onChange={(e) => setPostalCode(e.target.value)}
+        />
+
+        <label htmlFor="phoneNumber">
+          Phone Number <span></span>
+        </label>
+        <input
+          type="number"
+          id="phoneNumber"
+          className="form-control"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+
+        <label htmlFor="email">
+          Your Email <span className="required">*</span>
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <label htmlFor="password">
+          Password <span className="required">*</span>
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <label htmlFor="passwordConfirm">
+          Confirm Password <span className="required">*</span>
+        </label>
+        <input
+          type="password"
+          id="passwordConfirm"
+          className="form-control"
+          value={passwordConfirm}
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+          required
+        />
+
+        <button type="submit" className="btn btn-primary">
+          Register
+        </button>
       </form>
     </section>
   )
 }
 
 export default RegisterForm
-
 
 // const SignIn = () => {
 //   return (
--- webshop/src/firebase/utils.js
+++ webshop/src/firebase/utils.js
@@ -23,8 +23,45 @@
 //   }
 // }
 
+// import { initializeApp } from 'firebase/app'
+// import { getFirestore } from 'firebase/firestore'
+// import {
+//   getAuth,
+//   signInWithPopup,
+//   GoogleAuthProvider,
+//   createUserWithEmailAndPassword,
+// } from 'firebase/auth'
+// import { firebaseConfig } from './config'
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig)
+
+// export const db = getFirestore(app)
+// export const auth = getAuth(app)
+// export const provider = new GoogleAuthProvider()
+
+// export const signInWithGoogle = async () => {
+//   try {
+//     const result = await signInWithPopup(auth, provider)
+//     const user = result.user
+//     console.log(user)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// export const registerWithEmailAndPassword = async (email, password) => {
+//   try {
+//     const { user } = await createUserWithEmailAndPassword(auth, email, password)
+//     return user
+//   } catch (error) {
+//     console.log(error)
+//     throw error
+//   }
+// }
+
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore, collection, addDoc } from 'firebase/firestore'
 import {
   getAuth,
   signInWithPopup,
@@ -50,13 +87,24 @@ export const signInWithGoogle = async () => {
   }
 }
 
-export const registerWithEmailAndPassword = async (email, password) => {
+export const registerWithEmailAndPassword = async (
+  email,
+  password,
+  userData
+) => {
   try {
     const { user } = await createUserWithEmailAndPassword(auth, email, password)
+
+    // Create a new document in the "users" collection with the user's additional data
+    await addDoc(collection(db, 'users'), {
+      uid: user.uid,
+      email: user.email,
+      ...userData,
+    })
+
     return user
   } catch (error) {
     console.log(error)
     throw error
   }
 }
-
