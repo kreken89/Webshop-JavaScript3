2e60f6f767f7383ed9112c733a577590212fd34a
Elin <es.fredriksson@gmail.com>
2023-05-15 12:13:48 +0200

    contact form edit

--- webshop/src/pages/contact/Contact.jsx
+++ webshop/src/pages/contact/Contact.jsx
@@ -6,29 +6,29 @@ import React from 'react'
 const Contact = () => {
   return (
     <>
-    <section class="contact-wrap">
-  <form action="#" class="contact-form">
+    <section className="contact-wrap">
+  <form action="#" className="contact-form">
       <h3 className='title'>Contact us!</h3>
-    <div class="user-details">
-      <div class="input-box">
-        <label for="fullName">Your Name <span className='required'>*</span></label>
-        <input type="text" id="fullName" class="form-control" required/>
+    <div className="user-details">
+      <div className="input-box">
+        <label htmlFor="fullName">Your Name <span className='required'>*</span></label>
+        <input type="text" id="fullName" className="form-control" required/>
       </div>
-      <div class="input-box">
-        <label for="email">Your Email <span className='required'>*</span></label>
-        <input type="email" id="email" class="form-control" required/>
+      <div className="input-box">
+        <label htmlFor="email">Your Email <span className='required'>*</span></label>
+        <input type="email" id="email" className="form-control" required/>
       </div>
-      <div class="input-box">
-        <label for="phoneNumber">Phone Number <span className='required'>*</span></label>
-        <input type="number" id="phoneNumber" class="form-control" required/>
+      <div className="input-box">
+        <label htmlFor="phoneNumber">Phone Number <span className='required'>*</span></label>
+        <input type="number" id="phoneNumber" className="form-control" required/>
       </div>
-      <div class="input-box">
-        <label for="company">Company <span className='optinal'>(optinal)</span></label>
-        <input type="text" id="company" class="form-control"/>
+      <div className="input-box">
+        <label htmlFor="company">Company <span className='optinal'>(optinal)</span></label>
+        <input type="text" id="company" className="form-control"/>
       </div>
-      <div class="input-box">
-        <label for="message">Write something <span className='required'>*</span></label>
-        <textarea rows="6" type="text" id='message' class="form-control text-area" required></textarea>
+      <div className="input-box">
+        <label htmlFor="message">Write something <span className='required'>*</span></label>
+        <textarea rows="6" type="text" id='message' className="form-control text-area" required></textarea>
       </div>
     </div>
     <div className="terms">
@@ -40,7 +40,7 @@ const Contact = () => {
     
     
     
-      <button class="submit-btn">Submit</button>
+      <button className="submit-btn">Submit</button>
     
   </form>
 
