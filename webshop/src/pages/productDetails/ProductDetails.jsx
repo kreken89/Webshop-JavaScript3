3288ced10d193cd15e346c7c6e7b4a283897914d
DVNO <dvno@live.se>
2023-05-19 09:36:56 +0200

    xo

--- webshop/src/pages/productDetails/ProductDetails.jsx
+++ webshop/src/pages/productDetails/ProductDetails.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Details from '../../components/details/Details'
 import Carousel from '../../components/slider/Carousel'
-import SmallIcons from '../../components/smallIcons/SmallIcons'
 
 
 const ProductDetails = () => {
