59a45f048e69a250840bcf7248834fd66093be36
DVNO <dvno@live.se>
2023-05-21 13:56:55 +0200

    admin work

---
+++ webshop/src/components/adminPanel/AdminPanel.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const adminPanel = () => {
+  return (
+    <div className="login_sign_container">
+      <div className="login_container">
+        <h2>Welcome to Admin Panel</h2>
+        <div className="info_login"></div>
+        <hr />
+        
+      </div>
+    </div>
+  )
+}
+
+export default adminPanel
--- webshop/src/pages/admin/Admin.jsx
+++ webshop/src/pages/admin/Admin.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
+import AdminPanel from '../../components/adminPanel/AdminPanel'
 
 const Admin = () => {
   return (
-    <div className="login_sign_container">
-      <div className="login_container">
-        <h2>Welcome to Admin Panel</h2>
-        <div className="info_login"></div>
-      </div>
-    </div>
+    <>  
+     <AdminPanel />
+    </>
   )
 }
 
