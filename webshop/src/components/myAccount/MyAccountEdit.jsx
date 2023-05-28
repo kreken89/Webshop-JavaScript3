93114f6e95e2dbbbde38363b0e7bdbed4621a284
DVNO <dvno@live.se>
2023-05-28 11:42:14 +0200

    xo

--- webshop/src/components/myAccount/MyAccountEdit.jsx
+++ webshop/src/components/myAccount/MyAccountEdit.jsx
@@ -1,55 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { authReady } from '../../store/features/auth/authSlice'
+import authService from '../../store/features/auth/authService'
 
-const MyAccountEdit = () => {
+const MyAccount = () => {
   const { user } = useSelector((state) => state.auth)
   const dispatch = useDispatch()
 
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    address: '',
-    city: '',
-    postal_code: '',
-    phoneNumber: '',
-    email: '',
-  })
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value,
-    })
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    dispatch(update(formData))
-  }
-
   useEffect(() => {
-    if (user) {
-      const {
-        firstName = '',
-        lastName = '',
-        address = '',
-        city = '',
-        postal_code = '',
-        phoneNumber = '',
-        email = '',
-      } = user
-
-      setFormData({
-        firstName,
-        lastName,
-        address,
-        city,
-        postal_code,
-        phoneNumber,
-        email,
-      })
+    const fetchUserData = async () => {
+      try {
+        if (user) {
+          const userData = await authService.retrieveUserData(user.uid)
+          dispatch(authReady(userData))
+        }
+      } catch (error) {
+        console.log('Error retrieving user data:', error)
+      }
     }
-  }, [user])
+
+    fetchUserData()
+  }, [user, dispatch])
 
   if (!user) {
     return <div>Please log in to view your account</div>
@@ -58,87 +29,17 @@ const MyAccountEdit = () => {
   return (
     <div>
       <h2>My Account</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="input-box">
-          <label htmlFor="firstName">First Name</label>
-          <input
-            type="text"
-            id="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
-          />
-
-          <label htmlFor="lastName">Last Name</label>
-          <input
-            type="text"
-            id="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-          />
-
-          <label htmlFor="address">
-            Enter Your Address <span className="required">*</span>
-          </label>
-          <input
-            type="text"
-            id="address"
-            className="form-control"
-            value={formData.address}
-            onChange={handleChange}
-          />
-
-          <label htmlFor="city">
-            Enter Your City <span className="required">*</span>
-          </label>
-          <input
-            type="text"
-            id="city"
-            className="form-control"
-            value={formData.city}
-            onChange={handleChange}
-          />
-
-          <label htmlFor="postal_code">
-            Postal Code <span></span>
-          </label>
-          <input
-            type="text"
-            id="postal_code"
-            className="form-control"
-            value={formData.postal_code}
-            onChange={handleChange}
-          />
-
-          <label htmlFor="phoneNumber">
-            Phone Number <span></span>
-          </label>
-          <input
-            type="text"
-            id="phoneNumber"
-            className="form-control"
-            value={formData.phoneNumber}
-            onChange={handleChange}
-          />
-
-          <label htmlFor="email">
-            Your Email <span className="required">*</span>
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="form-control"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-
-          {/* Include other form fields for address, city, postal code, etc. */}
-
-          <button type="submit">Save</button>
-        </div>
-      </form>
+      <div>
+        <p>First Name: {user.firstName}</p>
+        <p>Last Name: {user.lastName}</p>
+        <p>Address: {user.address}</p>
+        <p>City: {user.city}</p>
+        <p>Postal Code: {user.postal_code}</p>
+        <p>Phone Number: {user.phoneNumber}</p>
+        <p>Email: {user.email}</p>
+      </div>
     </div>
   )
 }
 
-export default MyAccountEdit
+export default MyAccount
