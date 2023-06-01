0cab3e421f3663e01266fe54e1395c9fbe9d4351
Elin <es.fredriksson@gmail.com>
2023-06-01 10:48:40 +0200

    redirect

--- webshop/src/pages/myAccount/MyAccount.jsx
+++ webshop/src/pages/myAccount/MyAccount.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import MyAccountEdit from '../../components/myAccount/myAccountEdit'
-import MyAccountOrders from '../../components/myAccount/MyAccountOrders'
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import MyAccountEdit from '../../components/myAccount/myAccountEdit';
+import MyAccountOrders from '../../components/myAccount/MyAccountOrders';
 
 const MyAccount = () => {
-  return (
-    <div className="main_container">
-      <MyAccountEdit />
-      <MyAccountOrders />
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.auth.user);
 
-export default MyAccount
+  useEffect(() => {
+    if (!user || !user.uid) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
+
+  return (
+    <>
+      {user && user.uid && (
+        <div className="main_container">
+          <MyAccountEdit />
+          <MyAccountOrders />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default MyAccount;
